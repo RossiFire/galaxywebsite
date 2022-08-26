6e0330a25ffcf767ca2069ea6fb377165485453f
Daniele Rossino <daniele.rossino10@gmail.com>
2022-08-26 17:15:11 +0200

    Wip

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ export default function Home(){
         <div className="presentation-box">
 		<div> <SpecialTilt/></div>
 		<div>
-			<span>
+			<span>Test
 				A new <label>Galaxy</label> is about to be born. thousands of <label>lifes</label> are waiting to be lived. 
 				A new era of <label>Galaxy Life</label> is ready to start. 
 				All the starlings have to thanks the <label className="creator-label"><a href="https://phoenixnetwork.net/" rel="noreferrer"  target="_blank">Phoenix Network</a></label> for make the game really "reborn" and for make us feel the same emotions we felt some years ago.
